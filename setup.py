9bcd91fae84275323d0dd77fedb9faed30cf1e93
David Stanek <dstanek@dstanek.com>
2014-07-24 23:57:35 +0000

    Adds proper installation requirements

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 from distutils.core import setup
+import os
+
+
+HERE = os.path.dirname(__file__)
+
+
+def read_requirements(filename):
+    filename = os.path.join(HERE, filename)
+    return [line.strip() for line in open(filename) if line.strip()]
 
 
 setup(
@@ -9,5 +18,7 @@ setup(
     author='David Stanek',
     author_email='dstanek@dstanek.com',
     url='https://github.com/dstanek/jsd',
-    download_url='https://github.com/dstanek/jsd/tarball/0.1'
+    download_url='https://github.com/dstanek/jsd/tarball/0.1',
+    install_requires=read_requirements('requirements.txt'),
+    test_requires=read_requirements('test-requirements.txt')
 )
