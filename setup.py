a631f2387fd40dc43fced611ab8bd9d2b5b80344
David Stanek <dstanek@dstanek.com>
2014-07-25 01:37:04 +0000

    Bumps version to 0.3

--- setup.py
+++ setup.py
@@ -13,12 +13,12 @@ def read_requirements(filename):
 setup(
     name='jsd',
     packages=['jsd', 'jsd.tests'],
-    version='0.1',
+    version='0.3',
     description='Declarative JSONSchema generation.',
     author='David Stanek',
     author_email='dstanek@dstanek.com',
     url='https://github.com/dstanek/jsd',
-    download_url='https://github.com/dstanek/jsd/tarball/0.1',
+    download_url='https://github.com/dstanek/jsd/tarball/0.3',
     install_requires=read_requirements('requirements.txt'),
     tests_require=read_requirements('test-requirements.txt')
 )
